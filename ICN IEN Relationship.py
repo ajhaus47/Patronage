5c16d4a89a255cc90001981ac8959083e43d12e4
aaron.hauser@va.gov <aaron.hauser@va.gov>
2024-02-12 18:06:52 +0000

    Updates to Identity Correlations

--- ICN IEN Relationship.py
+++ ICN IEN Relationship.py
@@ -2,128 +2,145 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.window import Window
+from datetime import datetime
 from delta.tables import *
 
 # COMMAND ----------
 
 sorted_files = sorted(dbutils.fs.ls('/mnt/ci-mvi/Processed/SVeteran.SMVIPersonSiteAssociation/'), key=lambda x: x.modificationTime)
-last_modification_time = sorted_files[-1].modificationTime
+latest_modification_time = sorted_files[-1].modificationTime
+date_today = datetime.today().date()
 
-files_to_process = []
+ICUpdateRuns = spark.read.format("delta").load("/mnt/Patronage/Identity_Correlation_Run_History")
+ICUpdateRuns.createOrReplaceTempView('ICUpdateRuns_View')
 
-# for file in sorted_files:
-#     if file.modificationTime > unix_process_start_time:
-#         files_to_process.append(file.path)
-#     else:
-#         pass
+institution = spark.read.parquet("/mnt/ci-mvi/Raw/NDim.MVIInstitution/")
 
-# COMMAND ----------
-
-print(last_modification_time)
-
-# COMMAND ----------
-
-if DeltaTable.isDeltaTable(spark, "/mnt/Patronage/identity_correlations") is False:
-
-    status = "rebuild"
-
-    Window_Spec = Window.partitionBy(
+Window_Spec = Window.partitionBy(
         "MVIPersonICN",
         "MVITreatingFacilityInstitutionSID",
         "TreatingFacilityPersonIdentifier",
     ).orderBy(desc("CorrelationModifiedDateTime"))
 
-    person = (
-        spark.read.parquet("/mnt/ci-mvi/Processed/SVeteran.SMVIPersonSiteAssociation/")
-        .withColumn("rank", rank().over(Window_Spec))
-        .withColumnRenamed("CorrelationModifiedDateTime", "Last_Modified")
-        .filter(
-            (
-                (col("ActiveMergedIdentifierCode") == "A")
-                | (col("ActiveMergedIdentifierCode").isNull())
-            )
-            & (col("OpCode").isin("I", "U"))
-            & (col("MVITreatingFacilityInstitutionSID").isin(5667, 6061, 6722))
+# COMMAND ----------
+
+if DeltaTable.isDeltaTable(spark, "/mnt/Patronage/identity_correlations") is False:
+    status = "rebuild"
+    files_to_process = sorted_files
+else:
+    status = 'update'
+    unix_process_start_time  = spark.sql('select latest_modification_time from ICUpdateRuns_View order by latest_modification_time desc limit 1').collect()[0][0] 
+    files_to_process = []
+    for file in sorted_files:
+        if file.modificationTime > unix_process_start_time:
+            files_to_process.append(file.path)
+        else:
+            pass
+
+print(status)
+
+# COMMAND ----------
+
+person = (
+    spark.read.parquet(*files_to_process)
+    .withColumn("rank", rank().over(Window_Spec))
+    .withColumnRenamed("CorrelationModifiedDateTime", "Last_Modified")
+    .filter(
+        (
+            (col("ActiveMergedIdentifierCode") == "A")
+            | (col("ActiveMergedIdentifierCode").isNull())
         )
-        .select(
-            "MVIPersonICN",
-            "TreatingFacilityPersonIdentifier",
-            "MVITreatingFacilityInstitutionSID",
-            "calc_IngestionTimestamp",
-            "Last_Modified",
-            "rank",
-        )
-        .filter(col("rank") == 1)
+        & (col("OpCode").isin("I", "U"))
+        & (col("MVITreatingFacilityInstitutionSID").isin(5667, 6061, 6722))
     )
-
-    institution = spark.read.parquet("/mnt/ci-mvi/Raw/NDim.MVIInstitution/")
-
-    relationship_df = (
-        person.join(
-            institution,
-            person["MVITreatingFacilityInstitutionSID"]
-            == institution["MVIInstitutionSID"],
-            "left",
-        )
-        .select(
-            person["MVIPersonICN"],
-            institution["InstitutionCode"],
-            institution["MVIInstitutionSID"],
-            person["TreatingFacilityPersonIdentifier"],
-            person["Last_Modified"],
-        )
-        .distinct()
+    .select(
+        "MVIPersonICN",
+        "TreatingFacilityPersonIdentifier",
+        "MVITreatingFacilityInstitutionSID",
+        "calc_IngestionTimestamp",
+        "Last_Modified",
+        "rank",
     )
+    .filter(col("rank") == 1)
+)
 
-    duplicate_iens = (
-        relationship_df.groupBy("MVIPersonICN", "MVIInstitutionSID")
-        .count()
-        .filter(col("count") > 1)
-        .withColumnRenamed("MVIPersonICN", "ICN")
-        .withColumnRenamed("MVIInstitutionSID", "InstitutionSID")
-        .withColumnRenamed("count", "count_1")
+relationship_df = (
+    person.join(
+        institution,
+        person["MVITreatingFacilityInstitutionSID"]
+        == institution["MVIInstitutionSID"],
+        "left",
     )
-
-    duplicate_icns = (
-        relationship_df.groupBy("MVIInstitutionSID", "TreatingFacilityPersonIdentifier")
-        .count()
-        .filter(col("count") > 1)
-        .withColumnRenamed("MVIInstitutionSID", "InstitutionSID")
-        .withColumnRenamed("TreatingFacilityPersonIdentifier", "PersonIdentifier")
-        .withColumnRenamed("count", "count_2")
+    .select(
+        person["MVIPersonICN"],
+        institution["InstitutionCode"],
+        institution["MVIInstitutionSID"],
+        person["TreatingFacilityPersonIdentifier"],
+        person["Last_Modified"],
     )
+    .distinct()
+)
 
-    unduped_relationship_df = (
-        relationship_df.join(
-            duplicate_iens,
-            (relationship_df["MVIPersonICN"] == duplicate_iens["ICN"])
-            & (
-                relationship_df["MVIInstitutionSID"] == duplicate_iens["InstitutionSID"]
-            ),
-            "left",
-        )
-        .join(
-            duplicate_icns,
-            (relationship_df["MVIInstitutionSID"] == duplicate_icns["InstitutionSID"])
-            & (
-                relationship_df["TreatingFacilityPersonIdentifier"]
-                == duplicate_icns["PersonIdentifier"]
-            ),
-            "left",
-        )
-        .filter(col("count_1").isNull())
-        .filter(col("count_2").isNull())
-        .select(
-            relationship_df["MVIPersonICN"],
-            relationship_df["InstitutionCode"],
-            relationship_df["MVIInstitutionSID"],
-            relationship_df["TreatingFacilityPersonIdentifier"],
-            relationship_df["Last_Modified"],
-        )
+duplicate_iens = (
+    relationship_df.groupBy("MVIPersonICN", "MVIInstitutionSID")
+    .count()
+    .filter(col("count") > 1)
+    .withColumnRenamed("MVIPersonICN", "ICN")
+    .withColumnRenamed("MVIInstitutionSID", "InstitutionSID")
+    .withColumnRenamed("count", "count_1")
+)
+
+duplicate_icns = (
+    relationship_df.groupBy("MVIInstitutionSID", "TreatingFacilityPersonIdentifier")
+    .count()
+    .filter(col("count") > 1)
+    .withColumnRenamed("MVIInstitutionSID", "InstitutionSID")
+    .withColumnRenamed("TreatingFacilityPersonIdentifier", "PersonIdentifier")
+    .withColumnRenamed("count", "count_2")
+)
+
+unduped_relationship_df = (
+    relationship_df.join(
+        duplicate_iens,
+        (relationship_df["MVIPersonICN"] == duplicate_iens["ICN"])
+        & (
+            relationship_df["MVIInstitutionSID"] == duplicate_iens["InstitutionSID"]
+        ),
+        "left",
     )
+    .join(
+        duplicate_icns,
+        (relationship_df["MVIInstitutionSID"] == duplicate_icns["InstitutionSID"])
+        & (
+            relationship_df["TreatingFacilityPersonIdentifier"]
+            == duplicate_icns["PersonIdentifier"]
+        ),
+        "left",
+    )
+    .filter(col("count_1").isNull())
+    .filter(col("count_2").isNull())
+    .select(
+        relationship_df["MVIPersonICN"],
+        relationship_df["InstitutionCode"],
+        relationship_df["MVIInstitutionSID"],
+        relationship_df["TreatingFacilityPersonIdentifier"],
+        relationship_df["Last_Modified"],
+    )
+)
 
-    relationship_df.createOrReplaceTempView("relationship_df")
-    unduped_relationship_df.createOrReplaceTempView("unduped_relationship_df")
+relationship_df.createOrReplaceTempView("relationship_df")
+unduped_relationship_df.createOrReplaceTempView("unduped_relationship_df")
+
+# COMMAND ----------
+
+columns = ["run_date", "number_of_buckets", "latest_modification_time"]
+data = [(date_today, len(files_to_process), latest_modification_time)]
+rdd = spark.sparkContext.parallelize(data)
+df = rdd.toDF(columns)
+
+# COMMAND ----------
+
+if status == 'rebuild':
 
     icn_master = spark.sql("""
     with 200CORP as (
@@ -176,14 +193,32 @@ if DeltaTable.isDeltaTable(spark, "/mnt/Patronage/identity_correlations") is Fal
     )
     
 else:
-    status = "update"
     pass
-
-print(status)
+    print('pass')
 
 # COMMAND ----------
 
-if status = 'update':
-    pass
+if status == 'update':
+    identity_correlations = spark.read.format("delta").load("/mnt/Patronage/identity_correlations")
+
+    participant_id = unduped_relationship_df.filter(col("InstitutionCode") == '200CORP')
+    edipi = unduped_relationship_df.filter(col("InstitutionCode") == '200DOD')
+    va_profile_id = unduped_relationship_df.filter(col("InstitutionCode") == '200VETS')
 else:
     pass
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select MVIPersonICN, InstitutionCode, count(*) from relationship_df group by 1, 2 order by 3 desc;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from relationship_df where MVIPersonICN = 1008856884 and InstitutionCode = '200CORP'
+
+# COMMAND ----------
+
+display(spark.read.format('delta').load("/mnt/Patronage/CG_Run_History"))
